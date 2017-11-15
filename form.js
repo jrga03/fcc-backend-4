bde44b173d57c4c31ad5fc34bb3afa0fa277a1da
Jason Acido <jasonacido@gmail.com>
2017-11-15 03:44:56 +0000

    code that reverses form input

--- form.js
+++ form.js
@@ -1,4 +1,9 @@
 var express = require('express')
 var bodyparser = require('body-parser')
+var app = express()
 
-console.log(process.argv[2])
+app.use(bodyparser.urlencoded({extended: false}))
+app.post('/form', function(req, res) {
+    res.end(req.body.str.split('').reverse().join(''))
+})
+app.listen(process.argv[2])
